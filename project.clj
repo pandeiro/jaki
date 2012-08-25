b225cc7b3ea31a53b8ccafd3d0b39376c4af92df
Murphy McMahon <pandeiro@gmail.com>
2012-08-25 08:31:04 -0300

    added project.clj

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject jaki "0.1.0"
+  :description "A ClojureScript CouchDB client for the browser"
+  :url "https://github.com/pandeiro/jaki"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"})
